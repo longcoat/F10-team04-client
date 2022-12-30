10907b326ead6d278fee96ec760ea54c2eef06d6
kdh9585 <kdh9585@github.com>
2022-12-31 02:54:45 +0900

    footer 완료

--- src/component/commons/layout/footer/footer.tsx
+++ src/component/commons/layout/footer/footer.tsx
@@ -3,9 +3,35 @@ export default function LayoutFooter() {
   return (
     <S.Wrapper>
       <S.FooterHead>
-        <S.WrapperLeft>CODE CAMP.</S.WrapperLeft>
-        <S.WrapperRight></S.WrapperRight>
+        <S.WrapperLeft>
+          <S.LeftHead>CODE CAMP.</S.LeftHead>
+          <S.LeftMiddle>FOLLOW US ON</S.LeftMiddle>
+          <S.LeftIconWrap>
+            <S.LeftIcon src="./images/github.png"></S.LeftIcon>
+          </S.LeftIconWrap>
+        </S.WrapperLeft>
+        <S.WrapperRight>
+          <S.RightText>
+            <S.RightSubText>MEMBERS</S.RightSubText>
+            <S.RightSubText>CLUB</S.RightSubText>
+            <S.RightSubText>MESSAGE</S.RightSubText>
+            <S.RightSubText>GITHUB</S.RightSubText>
+          </S.RightText>
+          <S.RightText2>
+            <S.RightSubText>ABOUT</S.RightSubText>
+            <S.RightSubText>CONTACT</S.RightSubText>
+          </S.RightText2>
+        </S.WrapperRight>
       </S.FooterHead>
+      <S.FooterLine />
+      <S.FooterLast>
+        <S.FooterLeft>@BEDLAMS TM, ALL RIGHTS RESERVED</S.FooterLeft>
+        <S.FooterRight>
+          <S.LastText>COOKIE POLICY</S.LastText>
+          <S.LastText>LEGAL NOTICE</S.LastText>
+          <S.LastText>COOKIE CONSENT OPTIONS</S.LastText>
+        </S.FooterRight>
+      </S.FooterLast>
     </S.Wrapper>
   );
 }
