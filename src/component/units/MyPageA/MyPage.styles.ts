699acca0ad415200b0a3114dc94b6b867e2c42d0
longcoat <sherlockpkm@gmail.com>
2023-01-02 15:17:56 +0900

    Update 0102

---
+++ src/component/units/MyPageA/MyPage.styles.ts
@@ -0,0 +1,334 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 90px 0px;
+  margin: 0px auto;
+  width: 1400px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Wrapper = styled.div`
+  padding: 100px 200px;
+`;
+
+export const ProfileBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ProfileImageAvatarBox = styled.div``;
+
+export const ImageAvatar = styled.img``;
+
+export const ImageProfile = styled.img``;
+
+export const ImageHeart = styled.img``;
+
+export const InfoBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-left: 50px;
+`;
+
+export const NickInfoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+export const FriendHeartBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Nickname = styled.div`
+  font-family: "Code Next-Trial";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 44px;
+  line-height: 62px;
+  /* identical to box height, or 141% */
+
+  text-align: center;
+  letter-spacing: -0.002em;
+  padding-top: 20px;
+
+  color: #0b0b0b;
+`;
+
+export const Modify = styled.div`
+  font-family: "Code Next-Trial";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #8b8b8b;
+  padding: 45px 0 0 15px;
+`;
+
+export const FriendBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const HeartBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FameText = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #0b0b0b;
+  margin-left: 10px;
+  padding-top: 7px;
+`;
+
+export const FriendText = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #0b0b0b;
+  margin-left: 10px;
+  padding-top: 7px;
+`;
+
+export const FriendTextBox = styled.div``;
+
+export const FameTextBox = styled.div`
+  color: #8b8b8b;
+`;
+
+export const BoardBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FriendList = styled.div`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  /* identical to box height */
+
+  text-align: center;
+  letter-spacing: -0.002em;
+
+  color: #c71515;
+  padding-right: 90px;
+`;
+
+export const ChatList = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: -0.002em;
+
+  color: #0b0b0b;
+`;
+
+export const PickList = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 22px;
+  text-align: center;
+  letter-spacing: -0.002em;
+
+  color: #0b0b0b;
+  margin-right: 90px;
+`;
+
+export const FriendListBox = styled.div`
+  padding-left: 10px;
+`;
+
+export const ChatListBox = styled.div`
+  padding-right: 90px;
+`;
+
+export const PickListBox = styled.div``;
+
+export const BoardCategoryWrapper = styled.div`
+  padding: 0 300px 0 300px;
+`;
+export const Nim = styled.div`
+  font-family: "Code Next-Trial";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #8b8b8b;
+  padding-top: 45px;
+`;
+
+export const InformationBox = styled.div``;
+
+export const FollowFollowingBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const FollowingBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const FollowBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 0 0 0 60px;
+`;
+
+export const HeartBox1 = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const Items = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-bottom: 200px;
+`;
+
+export const ListContainer = styled.div`
+  padding-top: 60px;
+  background-color: blue;
+`;
+
+export const ReactIconBox = styled.div``;
+
+export const IconBox = styled.div`
+  color: #8b8b8b;
+`;
+export const RightIconUpBox = styled.div`
+  padding-left: 60px;
+  color: #8b8b8b;
+`;
+export const RightIconDownBox = styled.div`
+  padding-left: 90px;
+  color: #8b8b8b;
+`;
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡ보드리스트 부분ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+export const BoardListWrapper = styled.div`
+  padding: 50px 200px 0 200px;
+`;
+
+export const BoardList = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #8b8b8b;
+`;
+export const ImageListProfileBox = styled.div`
+  padding: 25px 35px 25px 0;
+`;
+
+export const ImageListProfile = styled.img`
+  width: 64px;
+`;
+
+export const InfoTextWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  padding: 25px 0 25px 0;
+`;
+
+export const InfoTextBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const Title = styled.div`
+  padding-right: 20px;
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #0b0b0b;
+`;
+
+export const Section = styled.div`
+  padding-right: 20px;
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #8b8b8b;
+`;
+
+export const Time = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 30px;
+  /* identical to box height, or 167% */
+
+  letter-spacing: -0.002em;
+
+  color: #8b8b8b;
+`;
+
+export const Content = styled.div`
+  font-family: "AppleSDGothicNeoM00";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 24px;
+  /* identical to box height */
+
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.0024em;
+
+  padding-top: 15px;
+
+  color: #0b0b0b;
+`;
+export const ThumbnailBox = styled.div`
+  width: 120px;
+  height: 90px;
+  padding-top: 20px;
+`;
+export const ThumbnailImage = styled.img``;
