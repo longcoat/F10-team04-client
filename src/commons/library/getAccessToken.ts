550eb83d58cd913ce082ae7698ba73dc1651fbc9
kdh9585 <kdh9585@github.com>
2023-01-05 20:58:17 +0900

    last

--- src/commons/library/getAccessToken.ts
+++ src/commons/library/getAccessToken.ts
@@ -11,7 +11,7 @@ const RESTORE_ACCESS_TOKEN = gql`
 export const getAccessToken = async () => {
   try {
     const graphQLClient = new GraphQLClient(
-      "https://https://meonjifather.shop/graphql",
+      "https://meonjifather.shop/graphql",
       { credentials: "include" }
     );
     const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
