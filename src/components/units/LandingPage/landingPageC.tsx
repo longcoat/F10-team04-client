51f228210ae05a3465f245e9ae855e12145a6eae
kdh9585 <kdh9585@github.com>
2023-01-10 16:11:16 +0900

    image

--- src/components/units/LandingPage/landingPageC.tsx
+++ src/components/units/LandingPage/landingPageC.tsx
@@ -22,12 +22,7 @@ export default function LandingPageC() {
             <S.Btn onClick={() => router.push("/community")}>COMMUNITY</S.Btn>
           </S.MemberBtn>
         </S.ContentsBox2>
-        <S.ImgBox
-          style={{
-            backgroundImage:
-              "url(https://s3-alpha-sig.figma.com/img/0b01/1b94/53c63c38a57e0c7ba3c25ef1d9afcfea?Expires=1673222400&Signature=Ht~LHolsZJt8-pkYHNh~BpLN3stllv2ApKLuySXjpvWfwwQ5EamPPrL6BImNkarWxyM~Z6FeTTSDhcbO4sbPBILua1jmlVSNMHhPKjHZyMbami~l8XR8ewyWdI-ygehoO9r5EfoEngCOYoCunHchmauQg7gwdbZH6Hs5a7VbSbj0-MEWTzTFPz2jWmZ5S1a7lfQkhfYXTJ1fAIEanmINRs-~93l4qbbgb2QQaKg1GuTO6d2ZUUVStVdFj2dokxp2HJwqGW7nxsMh~YP5jNtS~4LkhqRrnH3WoWc-GUnjAX7AanxzsNf-oJa9WGmunVSOqeDzLp5WQW~yQMRbuNQ5mQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)",
-          }}
-        ></S.ImgBox>
+        <S.ImgBox></S.ImgBox>
       </S.MainContents2>
     </S.Wrapper_2>
   );
