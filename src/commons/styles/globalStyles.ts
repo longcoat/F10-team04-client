ad0a5fffbe6296f541952fc17d6a8c2745715308
longcoat <sherlockpkm@gmail.com>
2023-01-20 11:19:01 +0900

    thursday morning update

--- src/commons/styles/globalStyles.ts
+++ src/commons/styles/globalStyles.ts
@@ -11,34 +11,32 @@ export const globalStyles = css`
   }
 
   html,
-body {
-  padding: 0;
-  margin: 0;
-  font-family: "AppleFont", "CodeNext",
-    Helvetica , sans-serif;
-}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
-@font-face {
-  font-family: "AppleFont";
-  src: url("/public/fonts/AppleFont/AppleSDGothicNeoM.ttf");
-}
-@font-face {
-  font-family: "CodeNext";
-  src: url("/public/fonts/CodeNext/Non-commercial/WEB/CodeNext-Trial-Regular.woff");
-}
-@media (prefers-color-scheme: dark) {
-  html {
-    color-scheme: dark;
-  }
   body {
-    color: white;
-    background: black;
+    padding: 0;
+    margin: 0;
+    font-family: "AppleFont", "CodeNext", Helvetica, sans-serif;
   }
-}
 
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @font-face {
+    font-family: "AppleFont";
+    src: url("/public/fonts/AppleFont/AppleSDGothicNeoM.ttf");
+  }
+  @font-face {
+    font-family: "CodeNext";
+    src: url("/public/fonts/CodeNext/Non-commercial/WEB/CodeNext-Trial-Regular.woff");
+  }
+  /* @media (prefers-color-scheme: dark) {
+    html {
+      color-scheme: dark;
+    }
+    body {
+      color: white;
+      background: black;
+    }
+  } */
 `;
